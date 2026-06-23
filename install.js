f4733ab0d9243f76134eeee6a774163173b77454
dell-optiplex-790 <dell.optiplex.d4719126@gmail.com>
2026-06-23 16:20:49 +0200

    fix install script

--- install.js
+++ install.js
@@ -7,7 +7,7 @@
     var xhr = new XMLHttpRequest();
     xhr.open('GET', 'https://dell-optiplex-790.github.io/moreapps/moreapps.js', false);
     xhr.send();
-    $store.set('/a/boot/moreapps.js', xhr.responseText);
-    $store.set('/a/pkg/README.txt', 'Drop in any package ZIPs here...');
+    $store.set('/boot/moreapps.js', xhr.responseText);
+    $store.set('/pkg/README.txt', 'Drop in any package ZIPs here...');
     location.reload();
 })();

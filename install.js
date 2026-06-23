91950af5a841a394171c3091ddb6cef141851bd0
dell-optiplex-790 <dell.optiplex.d4719126@gmail.com>
2026-06-23 15:56:13 +0200

    Add files via upload

--- install.js
+++ install.js
@@ -8,6 +8,8 @@
     xhr.open('GET', 'https://dell-optiplex-790.github.io/moreapps/moreapps.js', false);
     xhr.send();
     $file.save('/a/boot/moreapps.js', xhr.responseText, function() {
-        $file.save('/a/pkg/README.txt', 'Drop in any package ZIPs here...', location.reload);
+        $file.save('/a/pkg/README.txt', 'Drop in any package ZIPs here...', function() {
+            location.reload();
+        });
     });
 })();

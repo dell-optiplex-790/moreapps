b6085dfef6658212ab7cfd8eeb666bbde2bf22ca
dell-optiplex-790 <dell.optiplex.d4719126@gmail.com>
2026-06-23 15:30:05 +0200

    Add files via upload

---
+++ install.js
@@ -0,0 +1,13 @@
+/*!===================================================\
+|   dell's code                                       |
+|   MIT license - See LICENSE.txt for more details    |
+\====================================================*/
+
+(function install() {
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', 'https://dell-optiplex-790.github.io/moreapps/moreapps.js', false);
+    xhr.send();
+    $file.save('/a/boot/moreapps.js', xhr.responseText, function() {
+        $file.save('/a/pkg/README.txt', 'Drop in any package ZIPs here...', location.reload);
+    });
+})();
